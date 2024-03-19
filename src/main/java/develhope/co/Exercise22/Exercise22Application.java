1313c2f4886e8b62f6b7b3ae153511da93a43983
Giulio <giulio.marciante@hotmail.com>
2024-03-19 16:05:01 +0100

    first commit

---
+++ src/main/java/develhope/co/Exercise22/Exercise22Application.java
@@ -0,0 +1,13 @@
+package develhope.co.Exercise22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise22Application.class, args);
+	}
+
+}
---
+++ src/test/java/develhope/co/Exercise22/Exercise22ApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope.co.Exercise22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
